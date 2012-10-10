9900a943cefb7586c0cd008624ebbe32d80246c1
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-10-10 13:15:26 +0200

    Added config loading to index.

--- public/index.php
+++ public/index.php
@@ -2,6 +2,14 @@
 require '../vendor/autoload.php';
 
 use Jet\Router\Router;
+use Cabinet\DBAL\Db;
+use Kengai\Manager as Kengai;
+use Kengai\SourceReader\YAML;
+
+$config = new Kengai();
+
+$config->add(new YAML('../config.yml'));
+$config->fetch();
 
 $router = new Router;
 
