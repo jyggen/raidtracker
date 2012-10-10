174ad14c216e0663852ad3fae6c05496976ed3f8
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-10-10 12:13:03 +0200

    Added basic routing.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
 <?php
 require '../vendor/autoload.php';
+
+use Jet\Router\Router;
+
+$router = new Router;
+
+$router->addRoutes(array(
+	'/' => '\jyggen\raidtracker\Index:index'
+));
+
+$router->launch();
