7fe6aad1cccb851a886896103fddd829d8420eae
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-10-13 10:14:39 +0200

    Added index controller.

--- classes/Jyggen/Raidtracker/Controller/index.php
+++ classes/Jyggen/Raidtracker/Controller/index.php
@@ -1,9 +1,58 @@
 <?php
 namespace Jyggen\Raidtracker\Controller;
 
-class Index {
+use \Jyggen\Raidtracker\Controller;
 
-	public function index() {
+class Index extends Controller {
+
+	public function get_index() {
+
+		$events  = $this->db->select()->from('events')->orderBy('date', 'DESC')->execute();
+		$players = array();
+		$rows    = $this->db->select('p.id', array('c.name', 'class'), 'p.name', 'p.ilvl')
+		                ->from(array('players', 'p'))
+		                ->join(array('classes', 'c'), 'left')
+		                ->on('c.id', 'p.class_id')
+		                ->orderBy('p.name')
+		                ->execute();
+
+		foreach($rows as $row) {
+
+			$event_data = array();
+			$attendance = $this->db->select('a.id', 'a.status', array('e.id', 'event_id'))
+			                   ->from(array('attendance', 'a'))
+			                   ->join(array('events', 'e'), 'left')
+			                   ->on('e.id', 'a.event_id')
+			                   ->where('a.player_id', $row->id)
+			                   ->orderBy('e.date', 'DESC')
+			                   ->execute();
+
+			ksort($attendance);
+
+			foreach($attendance as $event) {
+
+				$event_data[$event->event_id] = array(
+					'id'     => $event->id,
+					'status' => $event->status,
+				);
+
+			}
+
+			$players[] = array(
+				'id'     => $row->id,
+				'class'  => $row->class,
+				'name'   => $row->name,
+				'ilvl'   => $row->ilvl,
+				'events' => $event_data,
+			);
+		}
+
+		krsort($events);
+
+		return $this->template->render('index.twig', array(
+			'events'  => $events,
+			'players' => $players,
+		));
 
 	}
 
