6969bf7c2380c02c9dfcf0dc1b835cc31cff1039
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-10-30 21:03:15 +0100

    Moved inline javascript to file.

---
+++ public/js/raidtracker.js
@@ -0,0 +1,43 @@
+$('#attendance').tooltip({
+  selector: "div[rel=tooltip]"
+});
+
+$('#login').on('click', function(){
+	navigator.id.request();
+});
+
+$('#logout').on('click', function(){
+	navigator.id.logout();
+});
+
+$('#addEventForm').on('submit', function(event) {
+	event.preventDefault();
+	$.ajax({
+		type   : 'POST',
+		url    : $(this).attr('action'),
+		data   : $(this).serialize(),
+		success: function(res, status, xhr) { alert("success"); },
+		error  : function(xhr, status, err) { alert("failure"); }
+	});
+});
+
+navigator.id.watch({
+	loggedInUser: currentUser,
+	onlogin: function(assertion) {
+		$.ajax({
+			type   : 'POST',
+			url    : '/login',
+			data   : {assertion: assertion},
+			success: function(res, status, xhr) { window.location.reload(); },
+			error  : function(xhr, status, err) { alert("login failure"); }
+		});
+	},
+	onlogout: function() {
+		$.ajax({
+			type   : 'POST',
+			url    : '/user/logout',
+			success: function(res, status, xhr) { /* window.location.reload();*/ },
+			error  : function(xhr, status, err) { alert("logout failure"); }
+		});
+	}
+});
