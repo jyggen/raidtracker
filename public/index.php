7612658a25cd982d12c064e6ffa0a18c32bbe035
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-10-30 22:58:39 +0100

    Moved event route to Event controller.

--- public/index.php
+++ public/index.php
@@ -28,63 +28,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->register(new Silex\Provider\SessionServiceProvider());
 $app->mount('/', new Jyggen\Raidtracker\Controller\Index());
 $app->mount('/user', new Jyggen\Raidtracker\Controller\User());
-
-$app->post('/event', function(Silex\Application $app, Symfony\Component\HttpFoundation\Request $request) {
-
-	$date = $request->request->get('date');
-
-	if(is_null($date) or date('Y-m-d', strtotime($date)) !== $date) {
-
-		$response = new Symfony\Component\HttpFoundation\Response();
-		$response->setStatusCode(400);
-
-		return $response;
-
-	}
-
-	$result = $app['db']->insert('events')->values(array(
-		'date' => $request->request->get('date'),
-	))->execute();
-
-	$event_id     = $result[0];
-	$valid_ids    = array();
-	$valid_status = array('OUT', 'STANDBY', 'OFFSPEC', 'CONFIRMED');
-
-	foreach($request->request->keys() as $key) {
-		if(substr($key, 0, 11) == 'attendance-' and in_array($request->request->get($key), $valid_status)) {
-			$valid_ids[] = substr($key, 11);
-		}
-	}
-
-	$players    = $app['db']->select('id')->from('players')->execute();
-	$player_ids = array();
-
-	foreach($players as $player) {
-		$player_ids[] = $player->id;
-	}
-
-	foreach($valid_ids as $key => $id) {
-		if(!in_array($id, $player_ids)) {
-			unset($valid_ids[$key]);
-		}
-	}
-
-	foreach($valid_ids as $id) {
-
-		$app['db']->insert('attendance')->values(array(
-			'player_id' => $id,
-			'event_id'  => $event_id,
-			'status'    => $request->request->get('attendance-'.$id)
-		))->execute();
-
-	}
-
-	$response = new Symfony\Component\HttpFoundation\Response();
-	$response->setStatusCode(200);
-
-	return $response;
-
-});
+$app->mount('/event', new Jyggen\Raidtracker\Controller\Event());
 
 $app->post('/login', function(Silex\Application $app, Symfony\Component\HttpFoundation\Request $request) {
 
