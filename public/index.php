93386cbc157a407f42983b0d8da9d00a1dac0d39
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-11-01 02:23:12 +0100

    Use aliasing for namespaces in index.

--- public/index.php
+++ public/index.php
@@ -2,22 +2,30 @@
 require '../vendor/autoload.php';
 require '../functions.php';
 
-$wisdom = new KevinGH\Wisdom\Wisdom('../');
-$wisdom->addLoader(new KevinGH\Wisdom\Loader\YAML);
+use Cabinet\DBAL\Db;
+use Jyggen\Raidtracker\Controller;
+use KevinGH\Wisdom\Loader\YAML;
+use KevinGH\Wisdom\Wisdom;
+use Silex\Application;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+
+$wisdom = new Wisdom('../');
+$wisdom->addLoader(new YAML);
 $wisdom->setCache('../cache');
 
-$app = new Silex\Application();
+$app = new Application;
 
 $app['config'] = $wisdom->get('config.yml');
 $app['debug']  = $app['config']['debug'];
-$app['db']     = Cabinet\DBAL\Db::connection(array(
+$app['db']     = Db::connection(array(
 	'driver'   => $app['config']['db']['driver'],
 	'username' => $app['config']['db']['username'],
 	'password' => $app['config']['db']['password'],
 	'database' => $app['config']['db']['database'],
 ));
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+$app->register(new TwigServiceProvider, array(
 	'twig.path'    => '../templates',
 	'twig.options' => array(
 		'debug' => $app['config']['debug'],
@@ -25,12 +33,12 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	),
 ));
 
-$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new SessionServiceProvider);
 
-$app->mount('/', new Jyggen\Raidtracker\Controller\Index());
-$app->mount('/drop', new Jyggen\Raidtracker\Controller\Drop());
-$app->mount('/event', new Jyggen\Raidtracker\Controller\Event());
-$app->mount('/item', new Jyggen\Raidtracker\Controller\Item());
-$app->mount('/user', new Jyggen\Raidtracker\Controller\User());
+$app->mount('/', new Controller\Index);
+$app->mount('/drop', new Controller\Drop);
+$app->mount('/event', new Controller\Event);
+$app->mount('/item', new Controller\Item);
+$app->mount('/user', new Controller\User);
 
 $app->run();
